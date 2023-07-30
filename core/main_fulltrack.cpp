29d47bc809076c7c80be2ae4269fb78bfa603624
Geordie Ross <rossgeordie@gmail.com>
2023-07-30 15:18:43 +0100

    Initial Commit

---
+++ core/main_fulltrack.cpp
@@ -0,0 +1,8 @@
+//
+//  main_fulltrack.cpp
+//  FullTrack
+//
+//  Created by Geordie Ross on 30/07/2023.
+//
+
+#include "main_fulltrack.hpp"
---
+++ core/main_fulltrack.hpp
@@ -0,0 +1,13 @@
+//
+//  main_fulltrack.hpp
+//  FullTrack
+//
+//  Created by Geordie Ross on 30/07/2023.
+//
+
+#ifndef main_fulltrack_hpp
+#define main_fulltrack_hpp
+
+#include <stdio.h>
+
+#endif /* main_fulltrack_hpp */
